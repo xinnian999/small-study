14ebce1d1958ad25c48ab1dae2129e4b1b4f4541
huyilin <v_huyilin@baidu.com>
2024-12-13 18:12:11 +0800

    ok

---
+++ pages/tabBar/Admin/index/index.vue
@@ -0,0 +1,91 @@
+<template>
+	<view>
+		<uni-card :is-shadow="false" is-full>
+			<text class="uni-h6">轻松管理，分配各种数据</text>
+		</uni-card>
+
+		<uni-section title="可管理项" type="line" padding>
+			<uni-grid :column="4" :highlight="true" @change="change">
+				<uni-grid-item v-for="(item, index) in menus" :index="index" :key="index">
+					<view class="grid-item-box" style="background-color: #fff;">
+						<uni-icons type="image" :size="30" color="#777" />
+						<text class="text">{{item.label}}</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</uni-section>
+	</view>
+</template>
+
+<script setup>
+	const menus=[
+		{label:'识图管理',path:''},
+		{label:'图库',path:''},
+		{label:'xx管理',path:''},
+		{label:'xx管理',path:''}
+	]
+</script>
+
+<style lang="scss">
+	.warp {
+		padding: 10px;
+	}
+
+	.button {
+		margin-bottom: 10px;
+	}
+
+	.image {
+		width: 25px;
+		height: 25px;
+	}
+
+	.text {
+		font-size: 14px;
+		margin-top: 5px;
+	}
+
+	.example-body {
+		/* #ifndef APP-NVUE */
+		// display: block;
+		/* #endif */
+	}
+
+	.grid-dynamic-box {
+		margin-bottom: 15px;
+	}
+
+	.grid-item-box {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-item-box-row {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-dot {
+		position: absolute;
+		top: 5px;
+		right: 15px;
+	}
+
+	.swiper {
+		height: 420px;
+	}
+</style>
---
+++ pages/tabBar/Home/index.vue
@@ -0,0 +1,49 @@
+<template>
+	<uni-section title="功能菜单" type="line">
+		<uni-list>
+			<uni-list-item title="识图" to="/pages/tabBar/Home/know" />
+			<uni-list-item title="识图错题集"  to="/pagestabBar/Home/know-errors" :rightText="knowStore.errors.length+ '个'" />
+			<uni-list-item title="重置数据" clickable @click="handleReset" />
+		</uni-list>
+	</uni-section>
+</template>
+
+<script setup>
+	import {
+		useKnowStore
+	} from '@/stores/know';
+	import COS from 'cos-js-sdk-v5';
+	
+	const knowStore = useKnowStore()
+	
+	const handleReset=()=>{
+		uni.clearStorage()
+	}
+	const cos = new COS({
+		SecretId: 'AKIDGNo5LTfppeFhy72zqUt8UhfdNuCIApFS',
+		SecretKey: '6xZxLVR8bsDSUZaR9f0B9wXv795BD6tZ',
+	});
+
+
+	cos.getBucket({
+		Bucket: 'quick-know-1305519392',
+		/* 填写自己的 bucket，必须字段 */
+		Region: 'ap-guangzhou',
+		/* 存储桶所在地域，必须字段 */
+		Prefix: 'images/',
+		/* 列出目录 a 下所有文件，非必须 */
+		Delimiter: '/'
+	}, function(err, data) {
+		// console.log(err || data.Contents);
+		const list= data.Contents.slice(1).map(item => ({
+			img: `https://quick-know-1305519392.cos.ap-guangzhou.myqcloud.com/${item.Key}`,
+			yes: 0,
+			no: 0
+		}))
+		knowStore.setDataSource(list)
+	});
+</script>
+
+<style>
+
+</style>
